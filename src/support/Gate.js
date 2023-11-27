2aae38c0e911c49a8aaa025b86b480424326e66a
Robert B <robert@planetweb.ro>
2023-11-27 19:08:12 +0200

    check if gate exists

--- src/support/Gate.js
+++ src/support/Gate.js
@@ -2,15 +2,18 @@ let self = {
     gates: {},
 
     allows(gate, param = null) {
+        self.check(gate);
         return self.gates[gate](param);
     },
 
     denies(gate, param = null) {
+        self.check(gate);
         return !self.gates[gate](param);
     },
 
     any(gates, param = null) {
         for ( let gate of gates ) {
+            self.check(gate);
             if ( self.gates[gate](param) ) {
                 return true;
             }
@@ -21,6 +24,7 @@ let self = {
 
     none(gates, param = null) {
         for ( let gate of gates ) {
+            self.check(gate);
             if ( self.gates[gate](param) ) {
                 return false;
             }
@@ -32,6 +36,12 @@ let self = {
     define(name, gate) {
         self.gates[name] = gate;
     },
+
+    check(gate) {
+        if ( typeof self.gates[gate] !== 'function' ) {
+            throw new Error(`Gate ${gate} does not exist.`);
+        }
+    }
 }
 
 export default self;
