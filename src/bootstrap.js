9681358a74c953b4b40cae0450ce8b8df7f99759
Robert B <robert@planetweb.ro>
2024-05-13 10:52:37 +0300

    boostrap refactor

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,50 +1,45 @@
 import Vue from 'vue';
 
 import './Mixins';
+import Alert from './Alert';
 import Cookie from './Support/Cookie';
-import Directives from './Support/Directives';
+import Confirm from './Confirm';
 import Domain from './Support/Domain';
 import Gate from './Support/Gate';
 import Helpers from './Support/Helpers';
 import Item from './Support/Item';
 import Items from './Support/Items';
+import Request from './Request';
 import StorageHandler from './Support/StorageHandler';
 
 import './Components';
 import './Directives';
 import './Mixins';
 
-import Alert from './Alert';
-import Confirm from './Confirm';
-import Request from './Request';
-
 let self = {
     run() {
+        window.Alert = Alert;
+        window.Cookie = Cookie;
+        window.Confirm = Confirm;
+        window.Domain = Domain;
+        window.Gate = Gate;
         window.Item = Item;
         window.Items = Items;
-
-        window.Cookie = Cookie;
-        window.Domain = Domain;
-        window.StorageHandler = StorageHandler;
-
-        window.Alert = Alert;
-        window.Confirm = Confirm;
         window.Request = Request;
+        window.StorageHandler = StorageHandler;
+        window.Str = Str;
 
         //make usable inside template
         Vue.prototype.Item = Item;
         Vue.prototype.Items = Items;
         Vue.prototype.Domain = Domain;
+        Vue.prototype.Gate = Gate;
+        Vue.prototype.Str = Str;
 
         //str helpers
-        window.Str = Str;
-        Vue.prototype.Str = Str;
         for ( let key in Str ) {
             Vue.filter(key, Str[key]);
         }
-
-        window.Gate = Gate;
-        Vue.prototype.Gate = Gate;
     }
 }
 
