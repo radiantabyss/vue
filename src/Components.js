3e94ff0e4f2c7fd3782ae063a3b4ba29e713541f
Robert B <robert@planetweb.ro>
2024-05-13 14:32:13 +0300

    trim fix

--- src/Components.js
+++ src/Components.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Str from './Support/Str';
 
 //default components
 import AlertComponent from './Components/AlertComponent';
@@ -17,7 +18,7 @@ for ( let i = 0; i < files.length; i++ ) {
         .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
         .toLowerCase();
 
-    name = window.trim(name, '-');
+    name = Str.trim(name, '-');
 
     Vue.component(name, context(files[i]).default);
 }
--- src/Directives.js
+++ src/Directives.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Str from './Support/Str';
 
 //default directives
 import ClosableDirective from './Directives/ClosableDirective';
@@ -22,7 +23,7 @@ for ( let i = 0; i < files.length; i++ ) {
         .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
         .toLowerCase();
 
-    name = window.trim(name, '-');
+    name = Str.trim(name, '-');
 
     Vue.directive(name, context(files[i]).default);
 }
