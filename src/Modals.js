8885d723efb3d174cefaaf72566e363ae182282c
Robert B <robert@planetweb.ro>
2024-05-15 17:55:35 +0300

    brought back modals

--- src/Modals.js
+++ src/Modals.js
@@ -1,13 +1,12 @@
 import Vue from 'vue';
 import Str from './Support/Str';
 
-let context = require.context('@/Components/', true, /\.vue/);
+let context = require.context(`@/Modals/`, true, /\.vue/);;
 let files = context.keys();
 
 for ( let i = 0; i < files.length; i++ ) {
     let split = files[i].split('/');
     let name = split[split.length - 1].replace('.vue', '')
-        .replace(/Component$/, '')
         .replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2')
         .toLowerCase();
 
