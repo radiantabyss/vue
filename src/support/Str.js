2cc09f8dd9af09424d0b6c48b06d473c065860f4
Robert Buchi <robert@planetweb.ro>
2025-12-27 01:40:45 +0200

    str random

--- src/support/Str.js
+++ src/support/Str.js
@@ -19,6 +19,15 @@ let self = {
         return encodeURIComponent(str);
     },
 
+    random(length) {
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let result = "";
+        for ( let i = 0; i < length; i++ ) {
+            result += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return result;
+    },
+
     slug(str) {
         str = str.replace(/^\s+|\s+$/g, ''); // trim
         str = str.toLowerCase();
